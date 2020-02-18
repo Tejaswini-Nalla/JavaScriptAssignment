2e64ae296851b9321198acf72145ea6ee1bcc8be
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-18 16:14:31 +0530

    Add reverse array script

---
+++ ReverseArray.js
@@ -0,0 +1,12 @@
+function reverseArray(values) {
+    var reversedArray = [];
+    for(index = values.length - 1; index >= 0; index--) {
+        reversedArray.push(values[index]);
+    }
+    return reversedArray;
+}
+
+{
+    let numbers = [1,2,5,6,8,12,34,5];
+    console.log(reverseArray(numbers));
+}
