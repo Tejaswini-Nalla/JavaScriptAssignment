56c5fefaed9a587642a580fe968d0e51d3bc0925
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-18 17:14:52 +0530

    Add NumbersAboveThreshold.js

---
+++ NumbersAboveThreshold.js
@@ -0,0 +1,14 @@
+function getCount(numbers, thresholdValue) {
+    var count = 0;
+    for(index = 0; index < numbers.length; index++) {
+        if(numbers[index] > thresholdValue)
+            count++;
+    }
+    return count;
+}
+
+{
+    let numbers = [2,41,7,9,10,34,25,33];
+    let thresholdValue = 5;
+    console.log(getCount(numbers,thresholdValue));
+}
