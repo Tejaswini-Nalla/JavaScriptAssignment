71ce8ed6fc7988ac4f46234cfe4d116976be8cd9
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-18 13:04:34 +0530

    Add SelectOddNumbers.js

---
+++ SelectOddNum.js
@@ -0,0 +1,21 @@
+function displayOddNumbers() {
+    console.log(oddNumbers);
+}
+
+function addToList(number) {
+    oddNumbers.push(number);
+}
+
+function isOddNum(number) {
+    return number%2!=0;
+}
+
+function checkOddNum(number) {
+    if(isOddNum(number))
+        addToList(number);
+}
+
+var numbers = [1,2,3,4,5,6,7,8,9];
+var oddNumbers = [];
+numbers.forEach(checkOddNum);
+displayOddNumbers();
