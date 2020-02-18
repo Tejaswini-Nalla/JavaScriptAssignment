44b24c563b4a6c3bcd51ceb7bbd358fdf9572ee1
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-18 14:26:32 +0530

    Add PrintingReverse.js

---
+++ PrintingReverse.js
@@ -0,0 +1,7 @@
+function displayNumbersInReverse() {
+    for(i = numbers.length - 1; i>=0; i--)
+        console.log(numbers[i]);
+}
+
+numbers = [1,2,3,4,9,20];
+displayNumbersInReverse();
--- sample.js
+++ sample.js
@@ -11,3 +11,11 @@ y++;
 console.log(x);
 console.log(y);
 */
+var a=10;
+var a=15;
+let b=5;
+let b=10;
+console.log(a);
+console.log(b);
+
+
