6f9462578c837f92f7d494b3d7aa372e082bb82f
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-19 17:41:09 +0530

    Add myfilter.js

---
+++ MyFunctions/myfilter.js
@@ -0,0 +1,18 @@
+function myfilter(mycallback,array) {
+    var returnValue = [];
+    for(let index = 0; index < array.length; index++) {
+        if(mycallback(array[index])) {
+            returnValue.push(array[index]);
+        }
+    }
+    return returnValue;
+}
+
+function isEven(number) {
+    return number%2 == 0;
+}
+
+{
+    const array = [1,2,3,4,5];
+    console.log(myfilter(isEven,array));
+}
