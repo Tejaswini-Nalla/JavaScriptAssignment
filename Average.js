3a5e4b9dff7d0267359f6aca3796d4858be3f9c6
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-21 11:03:36 +0530

    Modify Average.js

--- Average.js
+++ Average.js
@@ -10,3 +10,7 @@ var numbers = [1,2,3,4,5,6,7,8,9];
 var sum = 0;
 numbers.forEach(addNum);
 console.log(findAverage(numbers))
+
+//Using inbuilt function
+const result = numbers.reduce((acc,curvalue) => acc + curvalue);
+console.log(result/numbers.length);
