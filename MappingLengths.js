7cae83bf0da580480306a35f837aed5929c0f252
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-21 12:16:27 +0530

    Modify MapingLength.js

--- MappingLengths.js
+++ MappingLengths.js
@@ -7,7 +7,16 @@ function generateLength(name) {
     appendLength(len);
 }
 
-var names = ["teju","is","a","good","girl"];
-var lengths = [];
-names.forEach(generateLength)
-console.log(lengths);
+{
+    var names = ["teju","is","a","good","girl"];
+    var lengths = [];
+    names.forEach(generateLength)
+    console.log(lengths);
+
+    //Using built-in
+    const getLength = element => element.length;
+    console.log(names.map(getLength));
+}
+
+
+
