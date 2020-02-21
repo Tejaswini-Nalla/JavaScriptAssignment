bc6930bfc3aff856982807903de6dd70685387fa
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-21 11:08:37 +0530

    Modify BelowThreshold.js

--- BelowThreshold.js
+++ BelowThreshold.js
@@ -11,4 +11,8 @@ function getCount(numbers, thresholdValue) {
     let numbers = [2,41,7,9,10,34,25,33];
     let thresholdValue = 10;
     console.log(getCount(numbers,thresholdValue));
+
+    //Using builtin functions
+    const result = numbers.filter(number => number < thresholdValue);
+    console.log(result.length);
 }
