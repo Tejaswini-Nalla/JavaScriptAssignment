b614617151fa14b2913a137e842d62665a299b7c
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-21 11:27:08 +0530

    Modify SumOfNumbers.js

--- SumOfNumbers.js
+++ SumOfNumbers.js
@@ -1,4 +1,4 @@
-function displaySum() {
+function displaySum(sum) {
     console.log(sum);
 }
 
@@ -6,7 +6,13 @@ function addNum(number) {
     sum = sum + number;
 }
 
+{
 var numbers = [1,2,3,4,5,6,7,8,9];
 var sum = 0;
 numbers.forEach(addNum);
-displaySum();
+displaySum(sum);
+
+//using in-built functions
+const add = (accumulator, currentValue) => accumulator + currentValue;
+console.log(numbers.reduce(add));
+}
