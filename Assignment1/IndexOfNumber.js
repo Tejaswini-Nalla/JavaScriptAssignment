49d3ca1d2d73283e42e349f936bf2c5c56ae0b69
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-24 10:26:24 +0530

    Add findIndexOfNumber function in IndexOfNumber.js

--- Assignment1/IndexOfNumber.js
+++ Assignment1/IndexOfNumber.js
@@ -1,9 +1,20 @@
-function findIndex(number, numbers) {
+function findIndexOf(number, numbers) {
     for(index = 0; index < numbers.length; index++) {
         if(numbers[index] == number)
             return index;
     }
 }
 
-let numbers = [1,2,1,2,3,4,5,3];
-console.log(findIndex(3, numbers));
+function findIndexOfNumber(keyNumber, numbers) {
+    const isNumberEqual = element => element == keyNumber;
+    return numbers.findIndex(isNumberEqual);
+
+}
+
+function main() {
+    console.log(findIndexOf(3, [1,2,1,2,3,4,5,3]));
+    console.log(findIndexOfNumber(3, [1,2,1,2,3,4,5,3]));
+}
+
+main();
+
